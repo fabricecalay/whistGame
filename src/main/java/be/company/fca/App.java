1576cc68ebc4681dc97a389203cdc35fe4f3124f
fca <fabrice.calay@civadis.be>
2017-09-18 15:54:21 +0200

    spring boot application

--- src/main/java/be/company/fca/App.java
+++ src/main/java/be/company/fca/App.java
@@ -1,8 +1,16 @@
 package be.company.fca;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * First class of Whist Game
  * Created by fca on 18-09-17.
  */
+@SpringBootApplication
 public class App {
+
+    public static void main(String[] args){
+        SpringApplication.run(App.class, args);
+    }
 }
