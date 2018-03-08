81c585703513b420a1680a9659ac652c1d045e26
fabricecalay <fabrice.calay@gmail.com>
2018-03-08 20:15:22 +0100

    retour a spring-boot 1.5.10 car bug jdbc a investiguer pour spring-boot 2.0.0

--- src/main/java/be/company/fca/model/PointCarto.java
+++ src/main/java/be/company/fca/model/PointCarto.java
@@ -1,69 +1,69 @@
 package be.company.fca.model;
 
-import be.company.fca.utils.JsonToPointDeserializer;
-import be.company.fca.utils.PointToJsonSerializer;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.Point;
-import org.hibernate.annotations.Type;
+//import be.company.fca.utils.JsonToPointDeserializer;
+//import be.company.fca.utils.PointToJsonSerializer;
+//import com.fasterxml.jackson.annotation.JsonFormat;
+//import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+//import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+//import com.vividsolutions.jts.geom.Geometry;
+//import com.vividsolutions.jts.geom.Point;
+//import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "essai")
-@SequenceGenerator(name="essai_seq_id", initialValue=1, allocationSize=100)
+//@Entity
+//@Table(name = "essai")
+//@SequenceGenerator(name="essai_seq_id", initialValue=1, allocationSize=100)
 public class PointCarto {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "essai_seq_id")
-    private long id;
-
-    @Column
-    private String name;
-
-    @Column(name = "geom", columnDefinition = "geometry")
-    @JsonSerialize(using = PointToJsonSerializer.class)
-    @JsonDeserialize(using = JsonToPointDeserializer.class)
-    private Point geometry;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Point getGeometry() {
-        return geometry;
-    }
-
-    public void setGeometry(Point geometry) {
-        this.geometry = geometry;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        PointCarto that = (PointCarto) o;
-
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return (int) (id ^ (id >>> 32));
-    }
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "essai_seq_id")
+//    private long id;
+//
+//    @Column
+//    private String name;
+//
+//    @Column(name = "geom", columnDefinition = "geometry")
+//    @JsonSerialize(using = PointToJsonSerializer.class)
+//    @JsonDeserialize(using = JsonToPointDeserializer.class)
+//    private Point geometry;
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Point getGeometry() {
+//        return geometry;
+//    }
+//
+//    public void setGeometry(Point geometry) {
+//        this.geometry = geometry;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        PointCarto that = (PointCarto) o;
+//
+//        return id == that.id;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return (int) (id ^ (id >>> 32));
+//    }
 }
