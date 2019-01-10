01e1372d94d267467668c1719a6e75ea828cf464
fca <fabrice.calay@gmail.com>
2019-01-10 14:46:47 +0100

    listing des contrats (preparation)

--- src/main/java/be/company/fca/model/Contract.java
+++ src/main/java/be/company/fca/model/Contract.java
@@ -1,15 +1,54 @@
 package be.company.fca.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Contract {
 
+    //TODO : diagramme d'etat pour savoir vers quel contrat on peut passer en venant d'un autre
 
+    /**
+     * Permet de recuperer les contrats possibles sur base du contrat actuellement dominant
+     * et du contrat precedemment annonce par le joueur
+     * @param actualContract Contrat actuellement dominant
+     * @param previouslyAnnoncedContract Contrat precedemment annonce par le joueur
+     * @return
+     */
+    public List<Contract> getPossiblesContracts(Contract actualContract, Contract previouslyAnnoncedContract){
+        List<Contract> possiblesContracts = new ArrayList<Contract>();
+
+
+
+        return possiblesContracts;
+    }
+
+
+    enum ContractType {
+        EMBALLAGE,
+        SOLO,
+        TROU,
+        PETITE_MISERE,
+        HUIT_SUR_MISERE,
+        ABONDANCE,
+        GRANDE_MISERE,
+        GRANDE_MISERE_SUR_TABLE,
+        PETIT_CHELEM,
+        SOLO_CHELEM
+    }
 
     /**
      * Couleur de l'atout (s'il y en a un)
      */
     private Card.CardType trump;
 
+    /** Nombre de plis */
+    private Integer numberOfFolds;
+
     public Card.CardType getTrump() {
         return trump;
+    }
+
+    public Integer getNumberOfFolds() {
+        return numberOfFolds;
     }
 }
