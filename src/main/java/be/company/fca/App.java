46216e62ae0c143bda687b0b0f62f1cb35c5b776
fca <fabrice.calay@gmail.com>
2019-01-10 10:25:41 +0100

    premier essai jeu de la carte manuel

--- src/main/java/be/company/fca/App.java
+++ src/main/java/be/company/fca/App.java
@@ -7,13 +7,18 @@ import be.company.fca.model.Player;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -27,6 +32,7 @@ public class App extends Application {
     }
 
     private Game game;
+    private GridPane centralZone;
 
     @Override
     public void start(Stage primaryStage) {
@@ -39,13 +45,45 @@ public class App extends Application {
         // Affichage des decks
 
         primaryStage.setTitle("App");
-        GridPane root = new GridPane();
-        Scene scene = new Scene(root, Color.GRAY);
+        centralZone = new GridPane();
+        centralZone.setPadding(new Insets(15, 15, 15, 15));
+        centralZone.setAlignment(Pos.CENTER);
+
+        BorderPane border = new BorderPane();
+
+        Map<Player, Pane> paneMap = new HashMap<>();
+        HBox bottom = new HBox();
+        bottom.setPadding(new Insets(5, 15, 5, 15));
+        bottom.setAlignment(Pos.CENTER);
+        border.setBottom(bottom);
+        paneMap.put(game.getPlayers().get(0),bottom);
+
+        VBox left = new VBox();
+        left.setPadding(new Insets(15, 5, 15, 5));
+        left.setAlignment(Pos.CENTER);
+        border.setLeft(left);
+        paneMap.put(game.getPlayers().get(1),left);
+
+        HBox top = new HBox();
+        top.setPadding(new Insets(5, 15, 5, 15));
+        top.setAlignment(Pos.CENTER);
+        border.setTop(top);
+        paneMap.put(game.getPlayers().get(2),top);
+
+        VBox right = new VBox();
+        right.setPadding(new Insets(15, 5, 15, 5));
+        right.setAlignment(Pos.CENTER);
+        border.setRight(right);
+        paneMap.put(game.getPlayers().get(3),right);
+
+        border.setCenter(centralZone);
+
+        Scene scene = new Scene(border, Color.GRAY);
 
         int i = 0;
         for (Player player : game.getPlayers()){
             int j = 0;
-            root.add(new Label(player.getNickname()),i,j);
+//            root.add(new Label(player.getNickname()),i,j);
             j++;
             for (Card card : player.getPlayerDeck()){
                 Button btn = new Button();
@@ -61,7 +99,8 @@ public class App extends Application {
                             if (validCard){
                                 System.err.println("Carte jouee par " + player.getNickname() + " : " + card.getDisplayLabel());
                                 game.getCurrentFold().addCardToFold(game.getContract(),player, card);
-                                root.getChildren().remove(btn);
+                                paneMap.get(player).getChildren().remove(btn);
+                                playCard(player,card,game.getCurrentFold().getPlayedCards().size()==1);
                             }else{
                                 //System.err.println("Carte non-autorisée");
                             }
@@ -71,7 +110,7 @@ public class App extends Application {
 
                     }
                 });
-                root.add(btn,i,j);
+                paneMap.get(player).getChildren().add(btn);
                 j++;
             }
             i++;
@@ -82,11 +121,33 @@ public class App extends Application {
         primaryStage.show();
     }
 
+    public void playCard(Player player, Card card, boolean newFold){
+        if (newFold){
+            centralZone.getChildren().clear();
+        }
+        int columnIndex = 0;
+        int rowIndex = 0;
 
-    public static void maisn(String[] args) throws Exception{
-
-//        game.showPlayersDeck();
-
-//        game.launchCardGame();
+        switch (game.getPlayers().indexOf(player)){
+            case 0 :
+                columnIndex = 1;
+                rowIndex = 2;
+                break;
+            case 1 :
+                columnIndex = 0;
+                rowIndex = 1;
+                break;
+            case 2 :
+                columnIndex = 1;
+                rowIndex = 0;
+                break;
+            case 3 :
+                columnIndex = 2;
+                rowIndex = 1;
+                break;
+            default : break;
+        }
+        centralZone.add(new Label(card.getDisplayLabel()),columnIndex,rowIndex);
     }
+
 }
